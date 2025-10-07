a6e04185d2a49f30eedac5f2f9b9cc6bd387a879
ygorhslima <ygorlima.dev06@gmail.com>
2025-10-07 17:09:36 -0300

    deletando projetos

--- portfolio/src/components/ArticleProjetos.tsx
+++
@@ -1,7 +0,0 @@
-export default function ArticleProjetos(){
-    return (         
-        <article id="projetos">
-            <h2> <i className="fa-solid fa-trowel-bricks"></i> Projetos Criados</h2>
-        </article>
-    )
-}
