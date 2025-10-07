5751985ab2512a43bce1bb6a1f10cc7ff34d4107
ygorhslima <ygorlima.dev06@gmail.com>
2025-10-07 16:41:23 -0300

    Update vite.config.ts

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/ygor-henrique-portfolio"
+  base:"https://github.com/ygorhslima/ygor-henrique-portfolio/"
 })
