8babf6a3b6752ec26cc711aad88c176c17bfee92
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-10-09 08:03:56 -0300

    Update vite.config.js

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://ygorhslima.github.io/ygor-henrique-portfolio/",
+  base:"/ygor-henrique-portfolio/",
 })
